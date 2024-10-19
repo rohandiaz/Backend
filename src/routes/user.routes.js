c5711d9d1d2df1e2b4f5b927a658c5f75820d430
rohandiaz <rohanamylasedas@gmail.com>
2024-10-19 14:04:50 +0530

    Login and Logout done

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -19,4 +19,8 @@ router.route("/register").post(
     registerUser
     )
 
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
 export default router
